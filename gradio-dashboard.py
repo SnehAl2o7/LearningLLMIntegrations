9fa2949a3dc6fdd50ce9bcd8d068728e5da8963f
SnehAl2o7 <snehalsinghe353@gmail.com>
2025-10-19 01:14:54 +0530

    updated theme

--- gradio-dashboard.py
+++ gradio-dashboard.py
@@ -32,7 +32,7 @@ def retrieve_semantic(
 ) -> pd.DataFrame:
     
     recs = db_books.similarity_search(query, k=initial_top_k)
-    books_list = [int(rec.page_content.strip().split()[0]) for rec in recs]
+    books_list = [int(rec.page_content.strip('"').split()[0]) for rec in recs]
     book_recs = books[books["isbn13"].isin(books_list)].head(final_top_k)
 
     if category != "All":
@@ -96,27 +96,42 @@ def recommend_books(
 categories = ["All"] + sorted(books["simple_categories"].unique())
 tones = ["All"] + ["Happy", "Suprise", "Anger", "Suspenseful" , "Sad"]
 
+#the changing of theme
 
-with gr.Blocks(theme = gr.themes.Soft()) as dashboard:
+custom_theme = gr.themes.Glass().set(
+    primary_700="#7c0786", 
+    border_radius_xl="10px", 
+)
+
+with gr.Blocks(theme = custom_theme) as dashboard:
     gr.Markdown("# Semantic Book Recommender")
 
     with gr.Row():
-        user_query = gr.Textbox(label="Please enter a description of a book: ",
-                                placeholder= "e.g., A story about forgiveness")
+        gr.Column(scale=1) 
         
-        tone_dropdown = gr.Dropdown(choices=tones, label="Select an emotional tone:", value="All")
-        category_dropdown = gr.Dropdown(choices=categories, label= "Select a category:", value="All")
-        submit_button = gr.Button("Find Recommendations")
+        with gr.Column(scale=4):
+            gr.Markdown("## 📖 Enter Your Query and Preferences")
+            
+            user_query = gr.Textbox(label="Please enter a description of a book:",
+                                placeholder= "e.g., A story about a detective in 1920s London.")
+            
+            with gr.Row():
+                tone_dropdown = gr.Dropdown(choices=tones, label="Select an Emotional Tone:", value="All")
+                category_dropdown = gr.Dropdown(choices=categories, label= "Select a Category:", value="All")
+            
+            submit_button = gr.Button("🔍 Find Recommendations", variant="primary")
+            
+        gr.Column(scale=1)
+        
+    gr.Markdown("---") 
+    gr.Markdown("## ✨ Personalized Book Recommendations")
     
-    gr.Markdown("## Recommendation")
-    output = gr.Gallery(label="Recommmending books", columns=8, rows=2)
-
+    output = gr.Gallery(label="Recommmending books", columns=8, rows=2, object_fit="contain")
 
     submit_button.click(fn=recommend_books,
-                        inputs = [user_query, category_dropdown, tone_dropdown],
+                        inputs = [user_query, tone_dropdown, category_dropdown],
                         outputs= output)
     
-
 
 if __name__ == "__main__":
     dashboard.launch()
